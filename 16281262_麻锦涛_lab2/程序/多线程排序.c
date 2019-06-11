7f7b08c1efd2ec8cd80922fca7bfdf51e3c19c92
麻锦涛 <48515469+woshimajintao@users.noreply.github.com>
2019-06-11 22:51:48 +0800

    lab2

---
+++ 16281262_麻锦涛_lab2/程序/多线程排序.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<pthread.h>
+
+#define MAXNUM 10
+int originallist[MAXNUM]={7,12,19,3,18,4,2,6,15,8};
+int mergelist[MAXNUM];
+typedef struct
+{
+    int start;
+    int end;
+}Range;
+typedef struct
+{
+    Range r1;
+    Range r2;
+    pthread_t t1;
+    pthread_t t2;
+}MergeParam;
+static void workthread(void *param)
+{
+    Range *r=(Range*)param;
+    int i,j,t;
+    for(i=r->start;i<r->end;i++)
+        for(j=i+1;j<r->end;j++)
+        {
+            if(originallist[i]>originallist[j])
+            {
+                t=originallist[i];
+                originallist[i]=originallist[j];
+                originallist[j]=t;
+            }
+        }
+}
+static void mergethread(void *param)
+{
+    MergeParam *mp=(MergeParam*)param;
+    int i,j,k;
+    void *status;
+    
+    pthread_join(mp->t1,&status);
+    pthread_join(mp->t2,&status);
+    for(i=0,j=mp->r1.start,k=mp->r2.start;i<MAXNUM;i++)
+    {
+        if(j<mp->r1.end && k<mp->r2.end)
+        {
+            if(originallist[j]<originallist[k])
+            {
+                mergelist[i]=originallist[j];
+                j++;
+            }
+            else
+            {
+                mergelist[i]=originallist[k];
+                k++;
+            }
+        }
+        else if(j<mp->r1.end )
+        {
+            mergelist[i]=originallist[j];
+            j++;
+        }else
+        {
+            mergelist[i]=originallist[k];
+            k++;
+        }
+    }
+}
+int main(void)
+{
+    MergeParam mp;
+    pthread_t tid;
+    int i;
+    void *status;
+    
+    mp.r1.start=0;
+    mp.r1.end=5;
+    mp.r2.start=5;
+    mp.r2.end=10;
+    pthread_create(&(mp.t1),NULL,(void*(*)(void*))workthread,&(mp.r1));
+    pthread_create(&(mp.t2),NULL,(void*(*)(void*))workthread,&(mp.r2));
+    pthread_create(&tid,NULL,(void*(*)(void*))mergethread,&mp);
+    pthread_join(tid,&status);
+    for(i=0;i<MAXNUM;i++)
+        printf("%d ",mergelist[i]);
+    printf("\n");
+    return 0;
+}
+       
---
+++ 16281262_麻锦涛_lab2/程序/数独验证器.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<pthread.h>
+
+int sudo[9][9]=
+{
+    {6,2,4,5,3,9,1,8,7},
+    {5,1,9,7,2,8,6,3,4},
+    {8,3,7,6,1,4,2,9,5},
+    {1,4,3,8,6,5,7,2,9},
+    {9,5,8,2,4,7,3,6,1},
+    {7,6,2,3,9,1,4,5,8},
+    {3,7,1,9,5,6,8,4,2},
+    {4,9,6,1,8,2,5,7,3},
+    {2,8,5,4,7,3,9,1,6}
+};
+int status[27];
+typedef struct
+{
+    int left;
+    int top;
+    int right;
+    int buttom;
+    int index;
+}CheckRange;
+
+static void workthread(void *param)
+{
+    CheckRange *cr=(CheckRange*)param;
+    int i,j,k;
+    for(k=1;k<=9;k++)
+    {
+        for(i=cr->left;i<cr->right;i++)
+        {
+            for(j=cr->top;j<cr->buttom;j++)
+                if(k==sudo[i][j])
+                    break;
+            if(j<cr->buttom)
+                break;
+         }
+         if(i>=cr->right||j>=cr->buttom)
+            break;
+    }
+    if(k>9)
+        status[cr->index]=1;
+    else
+        status[cr->index]=2;
+}
+int main(void)
+{
+    CheckRange *cr;
+    pthread_t tid;
+    int i,j,index=0;
+    for(i=0;i<27;i++)
+        status[i]=0;
+    //check row
+    for(i=0;i<9;i++)
+    {
+        cr=(CheckRange *)malloc(sizeof(CheckRange));
+        cr->left=0;
+        cr->right=9;
+        cr->top=i;
+        cr->buttom=i+1;
+        cr->index=index++;
+        pthread_create(&tid,NULL,(void*(*)(void*))workthread,cr);
+    }
+     //check col
+    for(i=0;i<9;i++)
+    {
+        cr=(CheckRange *)malloc(sizeof(CheckRange));
+        cr->left=i;
+        cr->right=i+1;
+        cr->top=0;
+        cr->buttom=9;
+        cr->index=index++;
+        pthread_create(&tid,NULL,(void*(*)(void*))workthread,cr);
+    }   
+    //check 3*3
+    for(i=0;i<9;i+=3)
+    {
+        for(j=0;j<9;j+=3)
+        {
+            cr=(CheckRange *)malloc(sizeof(CheckRange));
+            cr->left=i;
+            cr->right=i+3;
+            cr->top=j;
+            cr->buttom=j+3;
+            cr->index=index++;
+            pthread_create(&tid,NULL,(void*(*)(void*))workthread,cr);
+        }
+    } 
+    i=0;
+    while(i<27)
+    {
+        if(status[i]==0)
+            continue;
+        if(status[i]==2)
+            break;
+        i++;
+    }
+    if(i<27)
+        printf("sudo is not match status[%d]=%d\n",i,status[i]);
+    else
+        printf("sudo is match\n");
+    return 0;
+}
+       
